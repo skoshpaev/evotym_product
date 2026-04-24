c3e4adce1a8160d389c6f6910a05ce92d685f0cb
Stanislav Koshpaev <arhatsv@gmail.com>
2026-04-24 09:19:54 +0200

    Use external shared bundle in product service

--- config/bundles.php
+++ config/bundles.php
@@ -4,5 +4,6 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Evotym\SharedBundle\EvotymSharedBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,10 +5,10 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Dto\CreateProductRequestDto;
-use App\Dto\ProductResponseDto;
 use App\Entity\Product;
 use App\Repository\ProductRepository;
 use App\Service\ProductServiceInterface;
+use Evotym\SharedBundle\Dto\ProductViewDto;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -24,7 +24,7 @@ final class ProductController
         $product = $productService->create($createProductRequestDto);
 
         return new JsonResponse(
-            ProductResponseDto::fromEntity($product)->toArray(),
+            ProductViewDto::fromProduct($product)->toArray(),
             Response::HTTP_CREATED,
         );
     }
@@ -33,7 +33,7 @@ final class ProductController
     public function list(ProductRepository $productRepository): JsonResponse
     {
         $data = array_map(
-            static fn (Product $product): array => ProductResponseDto::fromEntity($product)->toArray(),
+            static fn (Product $product): array => ProductViewDto::fromProduct($product)->toArray(),
             $productRepository->findAllOrderedByName(),
         );
 
@@ -43,6 +43,6 @@ final class ProductController
     #[Route('/{id}', name: 'product_show', methods: ['GET'], requirements: ['id' => '[0-9a-fA-F-]{36}'])]
     public function show(Product $product): JsonResponse
     {
-        return new JsonResponse(ProductResponseDto::fromEntity($product)->toArray());
+        return new JsonResponse(ProductViewDto::fromProduct($product)->toArray());
     }
 }
